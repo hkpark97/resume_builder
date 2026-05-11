e4997847c13ff8bd4cce3d5725943315df949d66
KatePark-Rogers <kate.park@rci.rogers.ca>
2026-05-11 14:02:12 -0400

    added vercel url in main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,7 +2,15 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth, public, saved
 app = FastAPI(title="Resumagic Service v4", version="0.4.0")
-app.add_middleware(CORSMiddleware, allow_origins=["http://127.0.0.1:5173", "http://localhost:5173"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+app.add_middleware(CORSMiddleware, 
+                   allow_origins=[
+                       "http://127.0.0.1:5173", 
+                       "http://localhost:5173",
+                       "https://resume-builder-dlghzigf9-kate-parks-projects-f882814e.vercel.app"
+                       ], 
+                   allow_credentials=True, 
+                   allow_methods=["*"], 
+                   allow_headers=["*"])
 app.include_router(public.router)
 app.include_router(auth.router)
 app.include_router(saved.router)
